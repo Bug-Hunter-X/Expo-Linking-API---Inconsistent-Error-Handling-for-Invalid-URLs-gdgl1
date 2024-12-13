b7809fb604d91140fa17ed64869eb6ef25cae2c7
BugHunter-X <askzenai@gmail.com>
2024-12-13 13:08:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo `Linking` API to open a URL and the URL is invalid or inaccessible.  The error message might not be very descriptive, making it difficult to debug.  This is often caused by typos in the URL, using a local URL that isn't accessible from the device, or the server at the URL being down.
